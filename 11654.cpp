25c630888b7fddec5873accf987b9e3f0a68e7dd
rrd0627 <39178521+rrd0627@users.noreply.github.com>
2020-05-05 15:20:05 +0900

    11654

---
+++ 11654.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char input;
+	cin >> input;
+	
+	int output;
+	output = input;
+
+	cout << output;
+
+	return 0;
+}
