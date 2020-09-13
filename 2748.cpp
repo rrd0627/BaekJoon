75213cfa1f5b640ef7452c012eb2c764c82b4b46
rrd0627 <rrd0627@gmail.com>
2020-09-13 15:53:09 +0900

    2748\

---
+++ 2748.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	long long dp[91] = { 0,};
+	
+	dp[1] = 1;	
+
+	for (int i = 2; i <= N; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[N];
+	
+
+	return 0;
+}
