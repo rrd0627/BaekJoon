dbb18f9cb51b9f4e24b7d45964f8a9c11a576d8c
rrd0627 <rrd0627@gmail.com>
2020-11-01 22:57:47 +0900

    1101

---
+++ 9655.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<queue>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	
+	if (N % 2 == 0)
+	{
+		cout << "CY";
+	}
+	else
+	{
+		cout << "SK";		
+	}
+
+
+	return 0;
+}
