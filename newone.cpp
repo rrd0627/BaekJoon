a91f6d7a2f2c72e84b38f876f9a9d04596d558f8
rrd0627 <rrd0627@gmail.com>
2020-12-12 01:59:40 +0900

    setup

---
+++ newone.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cin>>a;
+
+	cout<<a;
+	// int T;
+	// cin >> T;
+	
+	// int a,b;
+
+	// for(int i=0;i<T;i++)
+	// {
+	// 	cin>> a>>b;
+
+	// 	cout<<"Case #"<<i+1<< ": "<<a+b<<endl;
+	// }
+	// return 0;
+	return 0;
+}
